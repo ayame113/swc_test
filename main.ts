ef3ed8f22112342260d81dc1657cc60b0c904a2b
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-05-12 04:19:34 +0900

    work

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-import initSwc, { transformSync } from "https://esm.sh/@swc/wasm-web@1.3.57";
+import { transform } from "https://esm.sh/@swc/core@1.3.57";
 
-await initSwc();
-const result = transformSync(`console.log('hello')`, {});
-console.log(result);
+// import {} from "https://esm.sh/@swc/core@1.3.57/spack";
+
+// await initSwc();
+const result = await transform(`console.log('hello')`, {});
+console.log(result); // { code: 'console.log("hello");\n' }
